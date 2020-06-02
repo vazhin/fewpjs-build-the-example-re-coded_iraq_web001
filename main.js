c7dcf50376f3368e245795f6e8175e9bfc632cf1
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-02 18:03:15 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-
+let errorModal = document.querySelector('#modal')
 
 
 //------------------------------------------------------------------------------
